1070b57e14672ae56c8bf2636e64d974e5c3aa90
RaxelAgra <raxelagra@gmail.com>
2022-09-13 03:31:55 -0700

    Hello World

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
-#include<studio.h>
+#include<stdio.h>
 
 /**
- * main - Entry point
+ *  main - Entry point
  *
- * Description: A C program that prints with printf function.
+ *  Description: A C program that prints with printf function.
  *
- * Return: Always 0 (Success)
+ *  Return: Always 0 (Success)
 */
 
 int main(void)
