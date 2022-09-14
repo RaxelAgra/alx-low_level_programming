0ff9256a9e3a4f448b3bc5f80d7da49580b215f3
RaxelAgra <raxelagra@gmail.com>
2022-09-13 21:16:34 -0700

    Print alphabet in LC, then UC, then new LN

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,8 +3,7 @@
 /**
  * main - Entry point
  *
- * Description: print alphabets in lowercase
- * then in uppercase followed by a new line
+ * Description: print alphabet in lowercase then in uppercase
  *
  * Return: Always 0 (Success)
 */
