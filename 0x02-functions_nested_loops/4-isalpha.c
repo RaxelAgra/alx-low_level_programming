7a0b83978d5ca38af743abf0789e73176652b8bd
RaxelAgra <raxelagra@gmail.com>
2022-09-18 06:53:06 -0700

    Print the sign of a number

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,24 +1,25 @@
-#include"main.h"
+#include "main.h"
 
 /**
  * _isalph - Entry point
  * @c: the integer value it receives from other functions
- * Description: checks if character is a letter both lowercase and uppercase
+ *
+ * Description: check if character is a letter both lowercase and upercase
  * Return: 1 if true, 0 if false
-*/
+ */
 
 int _isalpha(int c)
 {
 	if (c >= 'a' && c <= 'z')
 	{
-		return (1);
-	}
+	return (1);
+}
 	else if (c >= 'A' && c <= 'Z')
 	{
-		return (1);
+	return (1);
 	}
 	else
 	{
-		return (0);
+	return (0);
 	}
 }
