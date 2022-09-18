890eceaa4cf20296fbdff17fbd062645f0e5819f
RaxelAgra <raxelagra@gmail.com>
2022-09-18 08:25:43 -0700

    Prints the 9 times table, starting with 0

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,7 +13,7 @@ void times_table(void)
 {
 	int num, mult, prod;
 
-	for (num = 0; num <+ 9; ++num)
+	for (num = 0; num <= 9; ++num)
 	{
 		_putchar(48);
 		for (mult = 1; mult <= 9; ++mult)
@@ -34,6 +34,6 @@ void times_table(void)
 
 			_putchar((prod % 10) + 48); /*get the second digit*/
 		}
-		_putchar('\n')
+		_putchar('\n');
 	}
 }
