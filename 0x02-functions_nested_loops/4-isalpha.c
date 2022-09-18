a928e1cf64829b86f69d024da38ad2d17cadfd10
RaxelAgra <raxelagra@gmail.com>
2022-09-18 03:58:28 -0700

    Print alphabets 10x in LC

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,24 @@
+#include"main.h"
+
+/**
+ * _isalph - Entry point
+ * @c: the integer value it receives from other functions
+ * Description: checks if character is a letter both lowercase and uppercase
+ * Return: 1 if true, 0 if false
+*/
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
